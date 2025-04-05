4c2db773d9ba9f97bf5448397231127f47ea65be
Yadickson Vasquez <yadicksonvasquez@gmail.com>
2025-04-05 12:49:05 -0400

    agregar mapper DTO y implementar servicio

--- WebApplicationSevenSuiteTest/services/EstadoCivilServiceImpl.cs
+++ WebApplicationSevenSuiteTest/services/EstadoCivilServiceImpl.cs
@@ -1,13 +1,24 @@
 ﻿using NLog;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using WebApplicationSevenSuiteTest.dto;
+using WebApplicationSevenSuiteTest.model;
+using WebApplicationSevenSuiteTest.model.repositories;
+using WebApplicationSevenSuiteTest.util;
 
 namespace WebApplicationSevenSuiteTest.services
 {
     public class EstadoCivilServiceImpl : IEstadoCivilService
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private IEstadoCivilRepository repository;
+
+        public EstadoCivilServiceImpl(IEstadoCivilRepository repository)
+        {
+            this.repository = repository;
+        }
 
         public int Add(EstadoCivilDTO entity)
         {
@@ -21,7 +32,16 @@ namespace WebApplicationSevenSuiteTest.services
 
         public IEnumerable<EstadoCivilDTO> Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+                IEnumerable<EstadoCivil> resultList = this.repository.Get();
+                return resultList.Select(DBMapperUtil.ToDTO).ToList();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+                throw;
+            }
         }
 
         public EstadoCivilDTO GetById(int Id)
---
+++ WebApplicationSevenSuiteTest/util/DBMapperUtil.cs
@@ -0,0 +1,25 @@
+﻿
+using WebApplicationSevenSuiteTest.dto;
+using WebApplicationSevenSuiteTest.model;
+
+namespace WebApplicationSevenSuiteTest.util
+{
+    public class DBMapperUtil
+    {
+        public static EstadoCivilDTO ToDTO(EstadoCivil entity) =>
+          new EstadoCivilDTO
+          {
+              Id = entity.Id,
+              Codigo = entity.Codigo,
+              Nombre = entity.Nombre
+          };
+
+        public static EstadoCivil ToEntity(EstadoCivilDTO dto) =>
+           new EstadoCivil
+           {
+               Id = dto.Id,
+               Codigo = dto.Codigo,
+               Nombre = dto.Nombre
+           };
+    }
+}
